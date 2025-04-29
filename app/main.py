c0fc1d6ae0cec8ea67506315203cb7ed02db6846
Asif <asif@192.168.1.108>
2025-04-29 16:57:02 +0530

    Fix import error by matching function names

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-def handler(event=None, context=None):
+def hello():
     print("Hello from Python CI/CD Pipeline!")
     return "Deployment Successful"
 
 if __name__ == "__main__":
-    handler()
+    hello()
 
